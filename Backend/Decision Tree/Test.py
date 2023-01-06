48c7f99428e451d0ab86c20e5adba0bea39eea91
Shane Adrian Lacsam <salacsam@ccc.edu.ph>
2023-01-06 22:01:38 +0800

    okay na

--- Backend/Decision Tree/Test.py
+++ Backend/Decision Tree/Test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import joblib
 
 
@@ -32,75 +33,6 @@ course_mapping = {
     'Bachelor of Secondary Education Major in Social Studies': 14
     # Add other possible course values and their indices here
 }
-# Create a mapping dictionary that maps each possible tuition fee value to its corresponding index
-tuition_fee_mapping = {
-    '12000': 0,
-    '13000': 1,
-    '14000': 2,
-    '16000': 3,
-    '17000': 4,
-    '18000': 5,
-    '19000': 6,
-    '20000': 7,
-    '21000': 8,
-    '22000': 9,
-    '23000': 10,
-    '24000': 11,
-    '25000': 12,
-    '26000': 13,
-    '32000': 14,
-    '33000': 15,
-    '34000': 16,
-    '38000': 17,
-    '40000': 18,
-    '41000': 19,
-    '42000': 20,
-    '43000': 21,
-    '44000': 22,
-    '45000': 23,
-    '46000': 24,
-    '47000': 25,
-    '48000': 26,
-    '49000': 27,
-    '50000': 28,
-    '51000': 29,
-    '52000': 30,
-    '53000': 31,
-    '54000': 32,
-    '55000': 33,
-    '56000': 34,
-    '57000': 35,
-    '58000': 36,
-    '59000': 37,
-    '60000': 38,
-    '61000': 39,
-    '62000': 40,
-    '63000': 41,
-    '64000': 42,
-    '65000': 43,
-    '70000': 44,
-    '71000': 45,
-    '72000': 46,
-    '73000': 47,
-    '74000': 48,
-    '75000': 49,
-    '76000': 50,
-    '77000': 51,
-    '78000': 52,
-    '79000': 53,
-    '80000': 54,
-    '81000': 55,
-    '82000': 56,
-    '83000': 57,
-    '84000': 58,
-    '85000': 59,
-    '86000': 60,
-    '87000': 61,
-    '88000': 62,
-    '89000': 63,
-    '90000': 64,
-    # Add other possible tuition fee values and their indices here
-}
 # Create a mapping dictionary that maps each possible location value to its corresponding index
 location_mapping = {
     'Brgy. Bucal': 0,
@@ -114,10 +46,23 @@ location_mapping = {
 
 encoded_input_values = [
     course_mapping[course],
-    tuition_fee_mapping[tuition_fee],
+    tuition_fee,
     location_mapping[location]
 ]
 #
-predictions = model.predict([encoded_input_values])
-# le.fit_transform(predictions)
-print(le.inverse_transform(predictions))
+# predictions = model.predict([encoded_input_values])
+# # le.fit_transform(predictions)
+# print(le.inverse_transform(predictions))
+
+# Make predictions on test data using the predict_proba method
+probabilities = model.predict_proba([encoded_input_values])
+
+# Get the top 3 classes (schools) for each data point
+top_5 = np.argsort(-probabilities, axis=1)[:, :5]
+
+# Flatten the array
+top_5_flat = top_5.flatten()
+
+# Convert the integer labels back into the original string labels
+recommended_schools = le.inverse_transform(top_5_flat).tolist()
+print(recommended_schools)
